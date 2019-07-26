0c3557b534fc63a54b139d3351966427dd7e85e6
jas623 <53236228+jas623@users.noreply.github.com>
2019-07-26 09:15:20 -0500

    Update friend.js

--- friend.js
+++ friend.js
@@ -1,35 +1,19 @@
-// function hideImages() {
-//   var button = document.getElementById("sample");
-//   button.innerHTML = "I've been clicked";
-// }
-//
-// function hideImages() {
-//   var button = document.getElementById("sample");
-//   button.innerHTML = "I've been clicked";
-//   var images = document.getElementByTagName("img");
-//   for (i=0; i<images.length; i++){
-//     images[i].style.display = "none";
-//   }
-// }
-//
-// function hideImages() {
-//   var button = document.getElementById("sample");
-//   button.innerHTML = "I've been clicked";
-//   var images = document.getElementById("ppl");
-//   images.style.display = "none";
-// }
-//
-// function hideCast() {
-//   var buttonCast = document.getElementById("meet");
-//   var cast = document.getElementById("ppl");
-//   cast.style.display = "none";
-// }
-//
+function imageEnlarge(x) {
+  x.style.height="400px";
+}
 
-function hideCast() {
-  var buttonCast = document.getElementById("meet");
-  var cast = document.getElementById("ppl");
-  for (i=0; i<cast.length; i++){
-    cast[i].style.display = "none";
+function imageShrink(y) {
+  y.style.height="250px";
+}
+
+var i = 0;
+var txt = 'Meet the Gang';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
   }
 }
